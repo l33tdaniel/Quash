c1db0e887bf9bc822ca5efc97f4189312ffaa4cc
Brett Balquist <brett@balquist.com>
2024-10-10 21:12:28 -0500

    Intial setup

---
+++ quash.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    //This runs until failure
+    while(1){
+        /* Directory line for quash */
+        printf("[QUASH]$    ");
+        fflush(stdout);
+        /* This is not working this is just an outline */
+
+    }
+}
